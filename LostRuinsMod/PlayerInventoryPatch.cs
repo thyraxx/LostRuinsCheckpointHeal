4c25667942a1f5170d29e414058ccf286cf6ef09
Thyraxx <taz3008@msn.com>
2025-05-03 20:43:29 +0200

    Added code for restoring HP/MP when touching checkpoint

---
+++ LostRuinsMod/PlayerInventoryPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Nunppong;
+using UnityEngine;
+
+namespace LostRuinsHealthCheckpoint
+{
+    [HarmonyPatch]
+    class PlayerInventoryPatch
+    {
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(AutoSave), "OnTriggerEnter2D")]
+
+        public static void HealOnAutoSaveCheckpoint(Collider2D collision)
+        {
+            if (collision.GetComponent<PlayableCharacter>() != null)
+            {
+                collision.GetComponent<PlayableCharacter>().Health.ResetHp();
+                collision.GetComponent<PlayableCharacter>().Health.ResetMp();
+            }
+        }
+    }
+}
---
+++ LostRuinsMod/StartPatch.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace LostRuinsHealthCheckpoint
+{
+    [BepInPlugin("org.thyraxx.lostruinshealcheckpoint", "Lost Ruins Heal Checkpoint", "0.0.1")]
+    public class StartPatch : BaseUnityPlugin
+    {
+        public const string pluginGuid = "org.thyraxx.lostruinshealcheckpoint";
+        Harmony harmony = new Harmony(pluginGuid);
+
+        public new static ManualLogSource Logger;
+        public void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo("Lostruinshealcheckpoint Loaded!");
+
+            harmony.PatchAll();
+        }
+
+        private void OnDestroy()
+        {
+            Logger.LogInfo("Lostruinshealcheckpoint Unloaded!");
+
+            // For hot-reload scriptEngine
+            harmony.UnpatchSelf();
+        }
+    }
+}
